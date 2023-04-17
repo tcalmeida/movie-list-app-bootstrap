435d1faf27e9f946bcc861e9ddd05d86a4e222e7
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-04-17 16:07:45 -0300

    refactor: change functions order for better readability

--- script.js
+++ script.js
@@ -4,22 +4,6 @@ async function getData() {
   return jsonData;
 }
 
-const movieList = getData();
-movieList.then((movies) => {
-  movies.forEach((movie) => {
-    const cardMovie = `
-    <div class="col-md-4 mt-4"  >
-    <div class="card shadow" style="width: 18rem; border: none">
-  <img src="${movie.avatar}" class="card-img-top" alt="...">
-  <div class="card-body ">
-    <p class="card-text text-center"> ${movie.titulo}</p>
-  </div>
-</div>
-    `;
-    document.querySelector('.movies').innerHTML += cardMovie;
-  });
-});
-
 const carousel = getData();
 carousel.then((items) => {
   const slide = `
@@ -51,4 +35,20 @@ carousel.then((items) => {
       </div>
     `;
   document.querySelector('.movies').innerHTML += slide;
+});
+
+const movieList = getData();
+movieList.then((movies) => {
+  movies.forEach((movie) => {
+    const cardMovie = `
+    <div class="col-md-4 mt-4"  >
+    <div class="card shadow" style="width: 18rem; border: none">
+  <img src="${movie.avatar}" class="card-img-top" alt="...">
+  <div class="card-body ">
+    <p class="card-text text-center"> ${movie.titulo}</p>
+  </div>
+</div>
+    `;
+    document.querySelector('.movies').innerHTML += cardMovie;
+  });
 });
