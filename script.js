56606c25288bee3da67f4b55e1d372fb9673a6ea
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-04-17 15:50:27 -0300

    build: create function to add first  3 movies on carousel

--- script.js
+++ script.js
@@ -19,3 +19,36 @@ movieList.then((movies) => {
     document.querySelector('.movies').innerHTML += cardMovie;
   });
 });
+
+const carousel = getData();
+carousel.then((items) => {
+  const slide = `
+      <div id="carouselExampleIndicators" class="carousel slide mt-4 d-flex ">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner ">
+          <div class="carousel-item active w-100 ">
+            <img src="${items[0].avatar}" class="d-block w-80" alt="...">
+          </div>
+          <div class="carousel-item w-100">
+            <img src="${items[1].avatar}" class="d-block w-80" alt="...">
+          </div>
+          <div class="carousel-item w-100">
+            <img src="${items[2].avatar}" class="d-block w-80" alt="...">
+          </div>
+        </div>
+        <button class="carousel-control-prev " type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+    `;
+  document.querySelector('.movies').innerHTML += slide;
+});
