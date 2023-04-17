47e6a5559531f00ea7400f64f981abc606068310
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-04-17 15:48:44 -0300

    build: create movie list looping to populate webpage

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 async function getData() {
-    const response = await fetch('https://api.b7web.com.br/cinema/');
-    const jsonData = await response.json();
-    return jsonData;
-  }
+  const response = await fetch('https://api.b7web.com.br/cinema/');
+  const jsonData = await response.json();
+  return jsonData;
+}
+
+const movieList = getData();
+movieList.then((movies) => {
+  movies.forEach((movie) => {
+    const cardMovie = `
+    <div class="col-md-4 mt-4"  >
+    <div class="card shadow" style="width: 18rem; border: none">
+  <img src="${movie.avatar}" class="card-img-top" alt="...">
+  <div class="card-body ">
+    <p class="card-text text-center"> ${movie.titulo}</p>
+  </div>
+</div>
+    `;
+    document.querySelector('.movies').innerHTML += cardMovie;
+  });
+});
